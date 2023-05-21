820988bd6f5f20a2e9f87073e87d9211cec1196b
Daniel <ayomide.daniel.adeosun@gmail.com>
2023-05-21 07:16:54 -0700

    mainloop.c

--- mainloop.c
+++ mainloop.c
@@ -8,38 +8,38 @@
  */
 int hsh(info_t *commandInfo, char **av)
 {
-    ssize_t r = 0;
-    int builtin_ret = 0;
+	ssize_t r = 0;
+	int builtin_ret = 0;
 
-    while (r != -1 && builtin_ret != -2)
-    {
-        clear_data(commandInfo);
-        if (isInteractive(commandInfo))
-            printString("$ ");
-        printErrorChar(BUF_FLUSH);
-        r = read_input(commandInfo);
-        if (r != -1)
-        {
-            set_data(commandInfo, av);
-            builtin_ret = detect_builtin(commandInfo);
-            if (builtin_ret == -1)
-                detect_cmd(commandInfo);
-        }
-        else if (isInteractive(commandInfo))
-            writeChar('\n');
-        clear_info(commandInfo, 0);
-    }
-    record_history(commandInfo);
-    clear_info(commandInfo, 1);
-    if (!isInteractive(commandInfo) && commandInfo->status)
-        exit(commandInfo->status);
-    if (builtin_ret == -2)
-    {
-        if (commandInfo->err_num == -1)
-            exit(commandInfo->status);
-        exit(commandInfo->err_num);
-    }
-    return (builtin_ret);
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_data(commandInfo);
+		if (isInteractive(commandInfo))
+			printString("$ ");
+		printErrorChar(BUF_FLUSH);
+		r = read_input(commandInfo);
+		if (r != -1)
+		{
+			set_data(commandInfo, av);
+			builtin_ret = detect_builtin(commandInfo);
+			if (builtin_ret == -1)
+				detect_cmd(commandInfo);
+		}
+		else if (isInteractive(commandInfo))
+			writeChar('\n');
+		clear_info(commandInfo, 0);
+	}
+	record_history(commandInfo);
+	clear_info(commandInfo, 1);
+	if (!isInteractive(commandInfo) && commandInfo->status)
+		exit(commandInfo->status);
+	if (builtin_ret == -2)
+	{
+		if (commandInfo->err_num == -1)
+			exit(commandInfo->status);
+		exit(commandInfo->err_num);
+	}
+	return (builtin_ret);
 }
 
 /**
@@ -47,32 +47,32 @@ int hsh(info_t *commandInfo, char **av)
  * @commandInfo: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 	0 if builtin was executed successfully,
- * 	1 if builtin was found but not successful,
- * 	2 if builtin has signals exit()
+ * 0 if builtin was executed successfully,
+ * 1 if builtin was found but not successful,
+ * 2 if builtin has signals exit()
  */
 int detect_builtin(info_t *commandInfo)
 {
-    int i, built_in_ret = -1;
-    builtin_table builtintbl[] = {
-        {"exit", shellExit},
-        {"env", showEnv},
-        {"help", showHelp},
-        {"history", showHistory},
-        {"setenv", updateEnv},
-        {"unsetenv", unsetEnv},
-        {"cd", changeDirectory},
-        {"alias", manageAlias},
-        {NULL, NULL}};
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", shellExit},
+		{"env", showEnv},
+		{"help", showHelp},
+		{"history", showHistory},
+		{"setenv", updateEnv},
+		{"unsetenv", unsetEnv},
+		{"cd", changeDirectory},
+		{"alias", manageAlias},
+		{NULL, NULL}};
 
-    for (i = 0; builtintbl[i].type; i++)
-        if (compareStrings(commandInfo->argv[0], builtintbl[i].type) == 0)
-        {
-            commandInfo->line_count++;
-            built_in_ret = builtintbl[i].func(commandInfo);
-            break;
-        }
-    return (built_in_ret);
+	for (i = 0; builtintbl[i].type; i++)
+		if (compareStrings(commandInfo->argv[0], builtintbl[i].type) == 0)
+		{
+			commandInfo->line_count++;
+			built_in_ret = builtintbl[i].func(commandInfo);
+			break;
+		}
+	return (built_in_ret);
 }
 
 /**
@@ -82,37 +82,36 @@ int detect_builtin(info_t *commandInfo)
  */
 void detect_cmd(info_t *commandInfo)
 {
-    char *path = NULL;
-    int i, k;
+	char *path = NULL;
+	int i, k;
 
-    commandInfo->path = commandInfo->argv[0];
-    if (commandInfo->linecount == 1)
-    {
-        commandInfo->line_count++;
-        commandInfo->linecount = 0;
-    }
-    for (i = 0, k = 0; commandInfo->arg[i]; i++)
-        if (!isDelimiter(commandInfo->arg[i], " \t\n"))
-            k++;
-    if (!k)
-        return;
-
-    path = searchPath(commandInfo, fetchEnv(commandInfo, "PATH="), commandInfo->argv[0]);
-    if (path)
-    {
-        commandInfo->path = path;
-        spawn_cmd(commandInfo);
-    }
-    else
-    {
-        if ((isInteractive(commandInfo) || fetchEnv(commandInfo, "PATH=") || commandInfo->argv[0][0] == '/') && isCmd(commandInfo, commandInfo->argv[0]))
-            spawn_cmd(commandInfo);
-        else if (*(commandInfo->arg) != '\n')
-        {
-            commandInfo->status = 127;
-            printErr(commandInfo, "not found\n");
-        }
-    }
+	commandInfo->path = commandInfo->argv[0];
+	if (commandInfo->linecount == 1)
+	{
+		commandInfo->line_count++;
+		commandInfo->linecount = 0;
+	}
+	for (i = 0, k = 0; commandInfo->arg[i]; i++)
+		if (!isDelimiter(commandInfo->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+	path = searchPath(commandInfo, fetchEnv(commandInfo, "PATH="), commandInfo->argv[0]);
+	if (path)
+	{
+		commandInfo->path = path;
+		spawn_cmd(commandInfo);
+	}
+	else
+	{
+		if ((isInteractive(commandInfo) || fetchEnv(commandInfo, "PATH=") || commandInfo->argv[0][0] == '/') && isCmd(commandInfo, commandInfo->argv[0]))
+			spawn_cmd(commandInfo);
+		else if (*(commandInfo->arg) != '\n')
+		{
+			commandInfo->status = 127;
+			printErr(commandInfo, "not found\n");
+		}
+	}
 }
 
 /**
@@ -123,35 +122,34 @@ void detect_cmd(info_t *commandInfo)
  */
 void spawn_cmd(info_t *commandInfo)
 {
-    pid_t child_pid;
+	pid_t child_pid;
 
-    child_pid = fork();
-    if (child_pid == -1)
-    {
+	child_pid = fork();
+	if (child_pid == -1)
+	{
         /* TODO: PUT ERROR FUNCTION */
-        perror("Error:");
-        return;
-    }
-    if (child_pid == 0)
-    {
-        if (execve(commandInfo->path, commandInfo->argv, access_env(commandInfo)) == -1)
-        {
-            clear_info(commandInfo, 1);
-            if (errno == EACCES)
-                exit(126);
-            exit(1);
-        }
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(commandInfo->path, commandInfo->argv, access_env(commandInfo)) == -1)
+		{
+			clear_info(commandInfo, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
         /* TODO: PUT ERROR FUNCTION */
-    }
-    else
-    {
-        wait(&(commandInfo->status));
-        if (WIFEXITED(commandInfo->status))
-        {
-            commandInfo->status = WEXITSTATUS(commandInfo->status);
-            if (commandInfo->status == 126)
-                printErr(commandInfo, "Permission denied\n");
-        }
-    }
+	}
+	else
+	{
+		wait(&(commandInfo->status));
+		if (WIFEXITED(commandInfo->status))
+		{
+			commandInfo->status = WEXITSTATUS(commandInfo->status);
+			if (commandInfo->status == 126)
+				printErr(commandInfo, "Permission denied\n");
+		}
+	}
 }
-
